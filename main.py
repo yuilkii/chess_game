9d6138968d7983d9cc2e131c82f052015003ed4a
yuilkii <74feinzyyy@gmail.com>
2023-01-20 17:05:15 +0300

    almost done

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pygame
 
 pos = []
+acttiveWindoow = 'Menu'
+
+
+class MainMenu:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def makeamenu(self):
+        brd = pygame.image.load('data/back.png')
+        cr = brd.get_rect(center=(width // 2, height // 2))
+        screen.blit(brd, cr)
 
 
 class Board:
@@ -28,6 +40,7 @@ class Board:
 
 class Piece:
     def __init__(self, piece_name, piece_coord, color1, path, is_alive=True):
+
         global positions, pos
         self.is_alive = is_alive
         self.piece_names = {
@@ -43,47 +56,47 @@ class Piece:
         self.path = path
         self.piece_name = piece_name
         self.cmove = []
-
-    def can_move(self):
+        self.rak = 0
         if self.color1 == 'white':
             self.clr = -1
         else:
             self.clr = 1
 
+    def can_move(self):
+
         if self.piece_name == 'knight':
             try:
                 self.drow_circle(self.piece_coord[0] + 2, self.piece_coord[1] - 1)  #
             except Exception:
-                print('error')
+                pass
             try:
-
                 self.drow_circle(self.piece_coord[0] - 2, self.piece_coord[1] - 1)  #
             except Exception:
-                print('error')
+                pass
             try:
                 self.drow_circle(self.piece_coord[0] - 2, self.piece_coord[1] + 1)  #
             except Exception:
-                print('error')
+                pass
             try:
                 self.drow_circle(self.piece_coord[0] + 2, self.piece_coord[1] + 1)  #
             except Exception:
-                print('error')
+                pass
             try:
                 self.drow_circle(self.piece_coord[0] - 1, self.piece_coord[1] - 2)  #
             except Exception:
-                print('error')
+                pass
             try:
                 self.drow_circle(self.piece_coord[0] + 1, self.piece_coord[1] - 2)  #
             except Exception:
-                print('error')
+                pass
             try:
                 self.drow_circle(self.piece_coord[0] - 1, self.piece_coord[1] + 2)
             except Exception:
-                print('error')
+                pass
             try:
                 self.drow_circle(self.piece_coord[0] + 1, self.piece_coord[1] + 2)  #
             except Exception:
-                print('error')
+                pass
 
         elif self.piece_name == 'rock':
             line_xm = 1
@@ -242,12 +255,7 @@ class Piece:
                 else:
                     break
 
-
         elif self.piece_name == 'king':
-
-            a = []
-
-            print(self.piece_coord)
 
             if self.piece_coord[1] > 1:
                 self.drow_circle(self.piece_coord[0], self.piece_coord[1] - 1)
@@ -273,53 +281,27 @@ class Piece:
             if self.piece_coord[0] < 8 and self.piece_coord[1] > 1:
                 self.drow_circle(self.piece_coord[0] + 1, self.piece_coord[1] - 1)
             if self.clr == -1:
-
                 if positions[8][8] == w_rock2:
 
-                    for i in range(6, 8):
-                        if positions[8][i] == '-':
-                            a.append('-')
-                    if all(a) and len(a) == 2:
+                    if positions[8][6] == '-' and positions[8][7] == '-':
                         self.drow_circle(self.piece_coord[0] + 2, self.piece_coord[1])
-
-                        w_rock.piece_coord = (6, 8)
-                        positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+                        self.rak = 1
 
                 if positions[8][1] == w_rock:
-
-                    for i in range(2, 5):
-                        if positions[8][i] == '-':
-                            a.append('-')
-
-                    if all(a) and len(a) == 3:
-                        w_rock2.piece_coord = (4, 8)
-                        positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+                    if positions[8][2] == '-' and positions[8][3] == '-' and positions[8][4] == '-':
                         self.drow_circle(self.piece_coord[0] - 2, self.piece_coord[1])
+                        self.rak = 2
             else:
-                a = []
-
                 if positions[1][8] == b_rock:
-                    print(3443)
                     for i in range(6, 8):
-                        if positions[1][i] == '-':
-                            a.append('-')
-                    if all(a) and len(a) == 2:
-                        print(132)
-                        self.drow_circle(self.piece_coord[0] + 2, self.piece_coord[1])
-                        b_rock2.piece_coord = (6, 1)
-                        positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+                        if positions[1][6] == '-' and positions[1][7] == '-':
+                            self.drow_circle(self.piece_coord[0] + 2, self.piece_coord[1])
+                            self.rak = 3
 
-                if positions[1][2] == b_rock2:
-
-                    for i in range(2, 5):
-                        if positions[1][i] == '-':
-                            a.append('-')
-                    print(a)
-                    if all(a) and len(a) == 3:
-                        print(112)
+                if positions[1][1] == b_rock2:
+                    if positions[1][2] == '-' and positions[1][3] == '-' and positions[1][4] == '-':
                         self.drow_circle(self.piece_coord[0] - 2, self.piece_coord[1])
-
-
+                        self.rak = 4
         elif self.piece_name == 'pawn':
             self.drow_circle(self.piece_coord[0], self.piece_coord[1] + self.clr)
             if self.piece_coord[1] == 2 and self.color1 == 'black':
@@ -336,22 +318,39 @@ class Piece:
             pos.append((x, y))
             pygame.draw.circle(screen, (255, 0, 0), (x * 100 - 20, y * 100 - 20), 20)
         if self.piece_name == 'pawn':
-            if positions[self.piece_coord[1] + self.clr][self.piece_coord[0] + 1] != '-' and \
-                    positions[self.piece_coord[1] + self.clr][self.piece_coord[0] + 1].color1 != self.color1:
-                pos.append((self.piece_coord[0] + 1, self.piece_coord[1] + self.clr))
-                pygame.draw.circle(screen, (255, 0, 0),
-                                   ((self.piece_coord[0] + 1) * 100 - 20, (self.piece_coord[1] + self.clr) * 100 - 20),
-                                   20)
-            if positions[self.piece_coord[1] + self.clr][self.piece_coord[0] - 1] != '-' and \
-                    positions[self.piece_coord[1] + self.clr][self.piece_coord[0] - 1].color1 != self.color1:
-                pos.append((self.piece_coord[0] - 1, self.piece_coord[1] + self.clr))
-                pygame.draw.circle(screen, (255, 0, 0),
-                                   ((self.piece_coord[0] - 1) * 100 - 20, (self.piece_coord[1] + self.clr) * 100 - 20),
-                                   20)
+            if self.piece_coord[0] < 8:
+                if positions[self.piece_coord[1] + self.clr][self.piece_coord[0] + 1] != '-' and \
+                        positions[self.piece_coord[1] + self.clr][self.piece_coord[0] + 1].color1 != self.color1:
+                    pos.append((self.piece_coord[0] + 1, self.piece_coord[1] + self.clr))
+                    pygame.draw.circle(screen, (255, 0, 0),
+                                       ((self.piece_coord[0] + 1) * 100 - 20,
+                                        (self.piece_coord[1] + self.clr) * 100 - 20),
+                                       20)
+            if self.piece_coord[0] > 1:
+                if positions[self.piece_coord[1] + self.clr][self.piece_coord[0] - 1] != '-' and \
+                        positions[self.piece_coord[1] + self.clr][self.piece_coord[0] - 1].color1 != self.color1:
+                    pos.append((self.piece_coord[0] - 1, self.piece_coord[1] + self.clr))
+                    pygame.draw.circle(screen, (255, 0, 0),
+                                       ((self.piece_coord[0] - 1) * 100 - 20,
+                                        (self.piece_coord[1] + self.clr) * 100 - 20),
+                                       20)
 
     def move(self, x1, y1):
         global chei_hod
         if (x1, y1) in pos:
+            if self.rak == 1:
+                w_rock2.piece_coord = (6, 8)
+                positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+            elif self.rak == 2:
+                positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+                w_rock.piece_coord = (4, 8)
+            elif self.rak == 3:
+                b_rock.piece_coord = (6, 1)
+                positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+            elif self.rak == 4:
+                b_rock2.piece_coord = (4, 1)
+                positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
+
             if positions[y1][x1] != '-':
                 positions[y1][x1].piece_coord = (0, 0)
             positions[self.piece_coord[1]][self.piece_coord[0]] = '-'
@@ -363,7 +362,7 @@ class Piece:
                 chei_hod = 'white'
 
 
-w_pawn1 = Piece('pawn', (2, 6), 'white', 'data/white_pawn.png')
+w_pawn1 = Piece('pawn', (1, 7), 'white', 'data/white_pawn.png')
 w_pawn2 = Piece('pawn', (2, 7), 'white', 'data/white_pawn.png')
 w_pawn3 = Piece('pawn', (3, 7), 'white', 'data/white_pawn.png')
 w_pawn4 = Piece('pawn', (4, 7), 'white', 'data/white_pawn.png')
@@ -384,10 +383,10 @@ w_rock2 = Piece('rock', (8, 8), 'white', 'data/white_rock.png')
 b_rock = Piece('rock', (8, 1), 'black', 'data/black_rock.png')
 b_rock2 = Piece('rock', (1, 1), 'black', 'data/black_rock.png')
 w_knight = Piece('knight', (2, 8), 'white', 'data/white_knight.png')
-w_knight2 = Piece('knight', (4, 5), 'white', 'data/white_knight.png')
+w_knight2 = Piece('knight', (7, 8), 'white', 'data/white_knight.png')
 b_knight = Piece('knight', (2, 1), 'black', 'data/black_knight.png')
 b_knight2 = Piece('knight', (7, 1), 'black', 'data/black_knight.png')
-w_bishop = Piece('bishop', (4, 4), 'white', 'data/white_bishop.png')
+w_bishop = Piece('bishop', (3, 8), 'white', 'data/white_bishop.png')
 w_bishop2 = Piece('bishop', (6, 8), 'white', 'data/white_bishop.png')
 b_bishop = Piece('bishop', (3, 1), 'black', 'data/black_bishop.png')
 b_bishop2 = Piece('bishop', (6, 1), 'black', 'data/black_bishop.png')
@@ -401,17 +400,17 @@ positions = [
     ['-', b_rock2, b_knight, b_bishop, b_queen, b_king, b_bishop2, b_knight2, b_rock],
     ['-', b_pawn1, b_pawn2, b_pawn3, b_pawn4, b_pawn5, b_pawn6, b_pawn7, b_pawn8],
     ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
-    ['-', '-', '-', '-', w_knight2, '-', '-', '-', '-'],
-    ['-', '-', '-', '-', w_bishop, '-', '-', '-', '-'],
     ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
-    ['-', '-', w_pawn2, w_pawn3, w_pawn4, w_pawn5, w_pawn6, w_pawn7, w_pawn8],
-    ['-', w_rock, w_knight, '-', w_queen, w_king, w_bishop2, '-', w_rock2],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', w_pawn1, w_pawn2, w_pawn3, w_pawn4, w_pawn5, w_pawn6, w_pawn7, w_pawn8],
+    ['-', w_rock, w_knight, w_bishop, w_queen, w_king, w_bishop2, w_knight2, w_rock2],
 ]
 
 pices = [b_rock2, b_knight, b_bishop, b_queen, b_king, b_bishop2, b_knight2, b_rock,
          b_pawn1, b_pawn2, b_pawn3, b_pawn4, b_pawn5, b_pawn6, b_pawn7, b_pawn8,
          w_knight2, w_bishop,
-         w_pawn2, w_pawn3, w_pawn4, w_pawn5, w_pawn6, w_pawn7, w_pawn8,
+         w_pawn1, w_pawn2, w_pawn3, w_pawn4, w_pawn5, w_pawn6, w_pawn7, w_pawn8,
          w_rock, w_knight, w_queen, w_king, w_bishop2, w_rock2]
 
 
@@ -425,6 +424,11 @@ def ya_eblan():
                 screen.blit(piece, ((j.piece_coord[0] - 1) * 100 + 30, (j.piece_coord[1] - 1) * 100 + 30))
 
 
+def game_over():
+    if (0, 0) == w_king.piece_coord or b_king.piece_coord == (0, 0):
+        print(1)
+
+
 chei_hod = 'white'
 flag = 0
 if __name__ == '__main__':
@@ -433,47 +437,52 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     running = True
     Board(1080, 1080).makeaboard()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                q, w = pygame.mouse.get_pos()
-                x, y = Board(width, height).get_coord(event.pos)
-                x1, y1 = Board(width, height).get_coord(event.pos)
-                x += 1
-                y += 1
-                if positions[y][x] != '-' and positions[y][x].color1 == chei_hod:
-                    flag = 1
-                    positions[y][x].can_move()
+            if acttiveWindoow == 'Menu':
+                MainMenu(width, height)
+            else:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    q, w = pygame.mouse.get_pos()
+                    x, y = Board(width, height).get_coord(event.pos)
+                    x1, y1 = Board(width, height).get_coord(event.pos)
+                    x += 1
+                    y += 1
+                    if positions[y][x] != '-' and positions[y][x].color1 == chei_hod:
+                        flag = 1
+                        positions[y][x].can_move()
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                q, w = pygame.mouse.get_pos()
-                x1, y1 = Board(width, height).get_coord(event.pos)
-                x1 += 1
-                y1 += 1
-                if positions[y][x] != '-' and positions[y][x].color1 == chei_hod:
-                    if y1 == y and x1 == x:
-                        pass
-                    else:
-                        positions[y][x].move(x1, y1)
-                        if chei_hod == 'white':
-                            chei_hod = 'black'
+                if event.type == pygame.MOUSEBUTTONUP:
+                    game_over()
+                    q, w = pygame.mouse.get_pos()
+                    x1, y1 = Board(width, height).get_coord(event.pos)
+                    x1 += 1
+                    y1 += 1
+                    if positions[y][x] != '-' and positions[y][x].color1 == chei_hod:
+                        if y1 == y and x1 == x:
+                            pass
                         else:
-                            chei_hod = 'white'
-                        pos = []
-                    flag = 0
-                Board(1080, 1080).makeaboard()
-
-            if flag == 1:
-                try:
+                            positions[y][x].move(x1, y1)
+                            if chei_hod == 'white':
+                                chei_hod = 'black'
+                            else:
+                                chei_hod = 'white'
+                            pos = []
+                        flag = 0
                     Board(1080, 1080).makeaboard()
-                    positions[y][x].can_move()
-                    piece = pygame.image.load(positions[y][x].path)
-                    screen.blit(piece, (event.pos[0] - 50, event.pos[1] - 50))
-                except:
-                    pass
 
-        ya_eblan()
+                if flag == 1:
+                    try:
+                        Board(1080, 1080).makeaboard()
+                        positions[y][x].can_move()
+                        piece = pygame.image.load(positions[y][x].path)
+                        screen.blit(piece, (event.pos[0] - 50, event.pos[1] - 50))
+                    except:
+                        pass
+
+            ya_eblan()
         pygame.display.update()
     pygame.quit()
